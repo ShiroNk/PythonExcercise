652201cb5bf8846996efea7f783dde1c493aea81
ShiroNk <39020200+ShiroNk@users.noreply.github.com>
2018-05-07 19:19:44 +0900

    「エラトステネスの篩」新規追加

---
+++ eratosthenes.py
@@ -0,0 +1,35 @@
+﻿# エラトステネスの篩
+def sieve_of_eratosthenes(x):
+    import math
+    prime_list = []
+
+    # 1. 探索リストに2から指定した値maxまでの整数を昇順で入れる。
+    search_list = [i for i in range(2, x + 1)]
+
+    # 3. 2の操作を探索リストの先頭値がmaxの平方根に達するまで行う。
+    search_end_num = math.sqrt(x)
+    while search_list[0] < search_end_num:
+        # 2. 探索リストの先頭の数を素数リストに移動し、その倍数を探索リストから削除する。
+        check_num = search_list[0]
+        prime_list.append(check_num)
+        for i in search_list:
+            if i % check_num == 0:
+                search_list.remove(i)
+
+    # 4. 探索リストに残った数を素数リストに移動する。
+    prime_list += search_list
+
+    return prime_list
+
+
+# 素数リストを取得して表示する
+import time
+start = time.time()
+
+prime_list = sieve_of_eratosthenes(10000)
+
+elapsed_time = time.time() - start
+print ("time:{0}".format(elapsed_time) + "[sec]")
+
+for prime in prime_list:
+    print(prime)
---
+++ eratosthenes_fast.py
@@ -0,0 +1,56 @@
+﻿def deco_save_table(func):
+    max_num = 0
+    table = []
+
+    def wrapper(num):
+        nonlocal max_num, table
+
+        if max_num < num:
+            max_num = num
+            table = func(num)
+            return table
+        elif max_num == num:
+            return table
+        else:
+            part_table = [n for n in table if n < num]
+            return part_table
+
+    return wrapper
+
+
+# エラトステネスの篩（高速版）
+@deco_save_table
+def sieve_of_eratosthenes_fast(x):
+    import math
+    judge_list = [True] * (x - 1)
+
+    search_end_num = math.sqrt(x)
+    check_num = 2
+    while check_num < search_end_num:
+        for n in range(check_num + 1, x + 1):
+            if n % check_num == 0:
+                judge_list[n - 2] = False
+        for n in range(check_num + 1, x + 1):
+            if judge_list[n - 2]:
+                check_num = n
+                break
+
+    prime_list = []
+    for i in range(2, len(judge_list)):
+        if judge_list[i - 2]:
+            prime_list += [i]
+
+    return prime_list
+
+
+# 素数リストを取得して表示する
+import time
+start = time.time()
+
+prime_list = sieve_of_eratosthenes_fast(100000)
+
+elapsed_time = time.time() - start
+print ("time:{0}".format(elapsed_time) + "[sec]")
+
+for prime in prime_list:
+    print(prime)
